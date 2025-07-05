13be90f0f26e1f37f6388899803b90f08d0c489a
saida9402 <saidahaymatova@gmail.com>
2025-07-05 21:34:23 +0900

    feat:restaurant login and signup  process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,6 +2,7 @@ import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
   private readonly memberModel;
@@ -15,6 +16,11 @@ class MemberService {
       .findOne({ memberType: MemberType.RESTAURANT })
       .exec();
     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+    console.log("before:", input.memberPassword);
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+    console.log("after:", input.memberPassword);
 
     try {
       const result = await this.memberModel.create(input);
@@ -33,7 +39,7 @@ class MemberService {
       )
       .exec();
     console.log("member:", member);
-    return member;
+    // return member;
     if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
 
     const isMatch = input.memberPassword === member.memberPassword;
