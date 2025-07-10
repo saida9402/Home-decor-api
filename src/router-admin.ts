a7c16959eac0868cb466836329ad20c7ff462c3c
saida9402 <saidahaymatova@gmail.com>
2025-07-10 20:15:17 +0900

    feat: create product controller and product routing api

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
+import productController from "./controllers/product.controller";
 
 /** Restaurant */
 routerAdmin.get("/", restaurantController.goHome);
@@ -14,6 +15,9 @@ routerAdmin.get("/logut", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSessions);
 
 /** Product */
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/:id", productController.updateChosenProduct);
 /** User */
 
 export default routerAdmin;
