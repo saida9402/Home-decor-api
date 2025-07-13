d4e1e4ef290a42920a2720219185972a8103b925
saida9402 <saidahaymatova@gmail.com>
2025-07-13 12:31:13 +0900

    feat: createNewProduct business logic by restaurant member

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,10 +1,27 @@
-import ProductModel from "src/schema/Product.model";
+import Errors from "../libs/Errors";
+import ProductModel from "../schema/Product.model";
+
+import { Product, ProductInput } from "../libs/types/product";
+import { HttpCode, Message } from "../libs/Errors";
 
 class ProductService {
   private readonly productModel;
 
   constructor() {
     this.productModel = ProductModel;
+  }
+
+  /**SPA */
+
+  /**SSR */
+
+  public async createNewProduct(input: ProductInput): Promise<Product> {
+    try {
+      return await this.productModel.create(input);
+    } catch (err) {
+      console.log("Error, model:createNewProduct:", err);
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    }
   }
 }
 
