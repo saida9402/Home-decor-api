4923a4c76ca02b8bf014700fa7b7a486ed343417
saida9402 <saidahaymatova@gmail.com>
2025-07-16 00:38:57 +0900

    feat: develop getUsers business logic

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -66,6 +66,15 @@ class MemberService {
     }
   }
 
+  public async getUsers(): Promise<Member[]> {
+    const result = await this.memberModel
+      .find({ memberType: MemberType.USER })
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+  }
+
   public async processLogin(input: LoginInput): Promise<Member> {
     const member = await this.memberModel
       .findOne(
