38f0d38332dcb731dd7dddc0d1795a6b4290139b
saida9402 <saidahaymatova@gmail.com>
2025-07-16 01:06:31 +0900

    feat: develop updateChosenUser api

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -90,9 +90,12 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
   }
 };
 
-restaurantController.updateChosenUser = (req: Request, res: Response) => {
+restaurantController.updateChosenUser = async (req: Request, res: Response) => {
   try {
     console.log("updateChosenUser ");
+    const result = await memberService.updateChosenUser(req.body);
+
+    res.status(HttpCode.OK).json({ data: result });
   } catch (err) {
     console.log("Error, updateChosenUser :", err);
     res.redirect("/admin");
