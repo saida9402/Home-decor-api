bd25bb9e8b4151f485ef2943df88ad50a64125dd
saida9402 <saidahaymatova@gmail.com>
2025-08-08 20:58:30 +0900

    feat: create middlewere methods and develop logout process

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -52,3 +52,10 @@ export interface AdminRequest extends Request {
   file: Express.Multer.File;
   files: Express.Multer.File[];
 }
+
+export interface ExtendedRequest extends Request {
+  member: Member;
+
+  file: Express.Multer.File;
+  files: Express.Multer.File[];
+}
