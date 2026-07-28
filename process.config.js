1d2a67d129a5dc1e2ce040d5d66a59bb14c04460
saida9402 <saidahaymatova@gmail.com>
2026-07-28 12:09:03 +0900

    chore(pm2): configure process.config.js for VPS deployment

--- process.config.js
+++ process.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   apps: [{
     name   : "HOME-DECOR",
-    cwd: "./",
+    cwd: "/home/home-decor-project",
     script : "./dist/server.js",
+    interpreter: "/root/.nvm/versions/node/v16.20.2/bin/node",
     watch: false,
     env_production: {
       NODE_ENV: "production"
@@ -11,6 +12,9 @@ module.exports = {
       NODE_ENV: "development"
     },
     instances: 1,
-    exec_mode: "cluster"
+    exec_mode: "fork",
+    max_memory_restart: "500M",
+    error_file: "/root/.pm2/logs/home-decor-error.log",
+    out_file: "/root/.pm2/logs/home-decor-out.log"
   }]
 }
