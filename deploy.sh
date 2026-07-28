52e37994402a9bba8e0ff1e6939cc387da8e6440
saida9402 <saidahaymatova@gmail.com>
2026-07-28 12:10:57 +0900

    chore(deploy): make deploy.sh idempotent and fail-fast

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+set -e
 
 # PRODUCTION
 git reset --hard
 git checkout master
 git pull origin master
 
-npm i
+npm ci
 npm run build
-pm2 start process.config.js --env production
+pm2 startOrRestart process.config.js --env production
+pm2 save
 
 
 # DEVELOPMENT bu zarur bo'lganda ishlatiladi shunchakiy yozilgan.
